29ba19163257749471fc5e51b363499e76128b0c
Dhanush D <dummydhan1scrap@gmail.com>
2026-05-08 15:06:16 +0530

    Uplaoding Project files and folders

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"expspl/data"
+	"expspl/handle"
+	"expspl/routes"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Env vr not loading")
+		return
+	}
+	db := data.Conndb()
+	h := &handle.Handler{DB: db}
+	mux := routes.SetupRoutes(h)
+	fmt.Println("Running on 8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
+}
